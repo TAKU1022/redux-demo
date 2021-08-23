7713165264b735dd711e6ca528f6f03c1497c274
Takumi <nase49946@gmail.com>
2021-08-23 21:56:28 +0900

    コンポーネント読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from './components/Button';
+import { Result } from './components/Result';
+
 export const App = () => {
   return (
-    <>
-      <div>
-        <p>App</p>
-      </div>
-    </>
+    <div>
+      <p>App</p>
+      <Button />
+      <Result />
+    </div>
   );
 };
