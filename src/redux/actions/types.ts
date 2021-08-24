b5931fba8f4e77443c7adaa80f71b9879592ee11
Takumi <nase49946@gmail.com>
2021-08-24 21:23:02 +0900

    actions type作成

---
+++ src/redux/actions/types.ts
@@ -0,0 +1,14 @@
+// 数字がクリックされたとき
+export const INPUT_NUMBER = 'INPUT_NUMBER';
+
+// 『+, -, ×, ÷』がクリックされたとき
+export const PULS = 'PULS';
+export const MINUS = 'MINUS';
+export const MULTIPLY = 'MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+
+// 『=』がクリックされたとき
+export const EQUAL = 'EQUAL';
+
+// 『AC』がクリックされたとき
+export const CLEAR = 'CLEAR';
