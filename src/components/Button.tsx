ea4d469b17768de70bf5abfb6b19d5ed26b60103
Takumi <nase49946@gmail.com>
2021-08-25 23:04:09 +0900

    props追加

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,17 @@ import { ReactNode, VFC } from 'react';
 type Props = {
   children: ReactNode;
   isOperator?: boolean;
+  onClick: () => void;
 };
 
 export const Button: VFC<Props> = (props: Props) => {
-  const { isOperator, children } = props;
+  const { isOperator, children, onClick } = props;
 
   return (
-    <button className={`button ${isOperator ? 'button--operator' : ''}`}>
+    <button
+      className={`button ${isOperator ? 'button--operator' : ''}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
